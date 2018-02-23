60b01167b37b4e1fb0b6eb21f881922dbcd26486
Sayful Islam <sayful.islam001@gmail.com>
2018-02-23 19:36:19 +0600

    Update WP Bootstrap Navwalker to version 3.0.0

--- functions.php
+++ functions.php
@@ -8,78 +8,78 @@
  */
 
 if ( ! function_exists( 'shapla_portfolio_setup' ) ) :
-/**
- * Sets up theme defaults and registers support for various WordPress features.
- *
- * Note that this function is hooked into the after_setup_theme hook, which
- * runs before the init hook. The init hook is too late for some features, such
- * as indicating support for post thumbnails.
- */
-function shapla_portfolio_setup() {
-	/*
-	 * Make theme available for translation.
-	 * Translations can be filed in the /languages/ directory.
-	 * If you're building a theme based on Shapla Portfolio, use a find and replace
-	 * to change 'shapla-portfolio' to the name of your theme in all the template files.
-	 */
-	load_theme_textdomain( 'shapla-portfolio', get_template_directory() . '/languages' );
-
-	// Add default posts and comments RSS feed links to head.
-	add_theme_support( 'automatic-feed-links' );
-
-	/*
-	 * Let WordPress manage the document title.
-	 * By adding theme support, we declare that this theme does not use a
-	 * hard-coded <title> tag in the document head, and expect WordPress to
-	 * provide it for us.
-	 */
-	add_theme_support( 'title-tag' );
-
-	/*
-	 * Enable support for Post Thumbnails on posts and pages.
+	/**
+	 * Sets up theme defaults and registers support for various WordPress features.
 	 *
-	 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+	 * Note that this function is hooked into the after_setup_theme hook, which
+	 * runs before the init hook. The init hook is too late for some features, such
+	 * as indicating support for post thumbnails.
 	 */
-	add_theme_support( 'post-thumbnails' );
-	set_post_thumbnail_size( 1170, 390, true );
-	add_image_size( 'portfolio-thumbnails', 370, 370, true );
+	function shapla_portfolio_setup() {
+		/*
+		 * Make theme available for translation.
+		 * Translations can be filed in the /languages/ directory.
+		 * If you're building a theme based on Shapla Portfolio, use a find and replace
+		 * to change 'shapla-portfolio' to the name of your theme in all the template files.
+		 */
+		load_theme_textdomain( 'shapla-portfolio', get_template_directory() . '/languages' );
 
-	// This theme uses wp_nav_menu() in one location.
-	register_nav_menus( array(
-		'primary' 		=> esc_html__( 'Primary Menu', 'shapla-portfolio' ),
-		'social-nav' 	=> esc_html__( 'Social Link Menu', 'shapla-portfolio' ),
-	) );
+		// Add default posts and comments RSS feed links to head.
+		add_theme_support( 'automatic-feed-links' );
 
-	/*
-	 * Switch default core markup for search form, comment form, and comments
-	 * to output valid HTML5.
-	 */
-	add_theme_support( 'html5', array(
-		'search-form',
-		'comment-form',
-		'comment-list',
-		'gallery',
-		'caption',
-	) );
+		/*
+		 * Let WordPress manage the document title.
+		 * By adding theme support, we declare that this theme does not use a
+		 * hard-coded <title> tag in the document head, and expect WordPress to
+		 * provide it for us.
+		 */
+		add_theme_support( 'title-tag' );
 
-	/*
-	 * Enable support for Post Formats.
-	 * See https://developer.wordpress.org/themes/functionality/post-formats/
-	 */
-	add_theme_support( 'post-formats', array(
-		'aside',
-		'image',
-		'video',
-		'quote',
-		'link',
-	) );
+		/*
+		 * Enable support for Post Thumbnails on posts and pages.
+		 *
+		 * @link https://developer.wordpress.org/themes/functionality/featured-images-post-thumbnails/
+		 */
+		add_theme_support( 'post-thumbnails' );
+		set_post_thumbnail_size( 1170, 390, true );
+		add_image_size( 'portfolio-thumbnails', 370, 370, true );
 
-	// Set up the WordPress core custom background feature.
-	add_theme_support( 'custom-background', apply_filters( 'shapla_portfolio_custom_background_args', array(
-		'default-color' => 'ffffff',
-		'default-image' => '',
-	) ) );
-}
+		// This theme uses wp_nav_menu() in one location.
+		register_nav_menus( array(
+			'primary'    => esc_html__( 'Primary Menu', 'shapla-portfolio' ),
+			'social-nav' => esc_html__( 'Social Link Menu', 'shapla-portfolio' ),
+		) );
+
+		/*
+		 * Switch default core markup for search form, comment form, and comments
+		 * to output valid HTML5.
+		 */
+		add_theme_support( 'html5', array(
+			'search-form',
+			'comment-form',
+			'comment-list',
+			'gallery',
+			'caption',
+		) );
+
+		/*
+		 * Enable support for Post Formats.
+		 * See https://developer.wordpress.org/themes/functionality/post-formats/
+		 */
+		add_theme_support( 'post-formats', array(
+			'aside',
+			'image',
+			'video',
+			'quote',
+			'link',
+		) );
+
+		// Set up the WordPress core custom background feature.
+		add_theme_support( 'custom-background', apply_filters( 'shapla_portfolio_custom_background_args', array(
+			'default-color' => 'ffffff',
+			'default-image' => '',
+		) ) );
+	}
 endif; // shapla_portfolio_setup
 add_action( 'after_setup_theme', 'shapla_portfolio_setup' );
 
@@ -93,6 +93,7 @@ add_action( 'after_setup_theme', 'shapla_portfolio_setup' );
 function shapla_portfolio_content_width() {
 	$GLOBALS['content_width'] = apply_filters( 'shapla_portfolio_content_width', 970 );
 }
+
 add_action( 'after_setup_theme', 'shapla_portfolio_content_width', 0 );
 
 /**
@@ -147,15 +148,16 @@ function shapla_portfolio_widgets_init() {
 		'after_title'   => '</h2>',
 	) );
 }
+
 add_action( 'widgets_init', 'shapla_portfolio_widgets_init' );
 
-if( !function_exists('is_shaplatools_activated') ):
-/**
- * Check ShaplaTools installed and activated
- */
-function is_shaplatools_activated(){
-	return (class_exists('ShaplaTools')) ? true : false;
-}
+if ( ! function_exists( 'is_shaplatools_activated' ) ):
+	/**
+	 * Check ShaplaTools installed and activated
+	 */
+	function is_shaplatools_activated() {
+		return ( class_exists( 'ShaplaTools' ) ) ? true : false;
+	}
 endif;
 
 
@@ -165,27 +167,33 @@ endif;
 function shapla_portfolio_scripts() {
 	wp_enqueue_style( 'shapla-portfolio-style', get_stylesheet_uri() );
 
-	wp_register_style( 'bootstrap', get_template_directory_uri() . '/assets/bootstrap/css/bootstrap.min.css', array('shapla-portfolio-style'), '3.3.4', 'all' );
+	wp_register_style( 'bootstrap', get_template_directory_uri() . '/assets/bootstrap/css/bootstrap.min.css', array( 'shapla-portfolio-style' ), '3.3.4', 'all' );
 
-	wp_register_script( 'bootstrap', get_template_directory_uri() . '/assets/bootstrap/js/bootstrap.min.js', array('jquery'), '3.3.4', true );
+	wp_register_script( 'bootstrap', get_template_directory_uri() . '/assets/bootstrap/js/bootstrap.min.js', array( 'jquery' ), '3.3.4', true );
 
 
-	wp_enqueue_script( 'plugins-js', get_template_directory_uri() . '/assets/js/plugins.js', array('jquery'), '1.0.0', true );
-	wp_enqueue_script( 'main-js', get_template_directory_uri() . '/assets/js/main.js', array('jquery'), '1.0.0', true );
+	wp_enqueue_script( 'plugins-js', get_template_directory_uri() . '/assets/js/plugins.js', array( 'jquery' ), '1.0.0', true );
+	wp_enqueue_script( 'main-js', get_template_directory_uri() . '/assets/js/main.js', array( 'jquery' ), '1.0.0', true );
 
 	// Load the html5 shiv.
-	// wp_enqueue_script( 'shapla-portfolio-html5', get_template_directory_uri() . '/assets/js/html5.js', array(), '3.7.3' );
-	// wp_script_add_data( 'shapla-portfolio-html5', 'conditional', 'lt IE 9' );
+	wp_enqueue_script( 'shapla-portfolio-html5', get_template_directory_uri() . '/assets/js/html5.js', array(), '3.7.3' );
+	wp_script_add_data( 'shapla-portfolio-html5', 'conditional', 'lt IE 9' );
 
-	if ( !is_shaplatools_activated() ) {
+	if ( ! is_shaplatools_activated() ) {
 
-		wp_register_style( 'font-awesome', get_template_directory_uri() . '/assets/font-awesome/css/font-awesome.css' , '', '4.1.0', 'all' );
+		wp_register_style( 'font-awesome', get_template_directory_uri() . '/assets/font-awesome/css/font-awesome.css', '', '4.1.0', 'all' );
 
-		wp_register_script( 'modernizr', get_template_directory_uri(). '/assets/shuffle/jquery.shuffle.modernizr.min.js', array(), '3.2', true );
-		wp_register_script( 'shuffle', get_template_directory_uri(). '/assets/shuffle/jquery.shuffle.min.js', array( 'jquery', 'modernizr' ), '3.2', true );
+		wp_register_script( 'modernizr', get_template_directory_uri() . '/assets/shuffle/jquery.shuffle.modernizr.min.js', array(), '3.2', true );
+		wp_register_script( 'shuffle', get_template_directory_uri() . '/assets/shuffle/jquery.shuffle.min.js', array(
+			'jquery',
+			'modernizr'
+		), '3.2', true );
 
 	}
-	wp_register_script( 'shuffle-custom', get_template_directory_uri(). '/assets/shuffle/shuffle-custom.js', array( 'jquery', 'shuffle' ), '3.2', true );
+	wp_register_script( 'shuffle-custom', get_template_directory_uri() . '/assets/shuffle/shuffle-custom.js', array(
+		'jquery',
+		'shuffle'
+	), '3.2', true );
 
 	// Enqueue Styles & Scripts
 	wp_enqueue_style( 'font-awesome' );
@@ -202,17 +210,19 @@ function shapla_portfolio_scripts() {
 		wp_enqueue_script( 'comment-reply' );
 	}
 }
+
 add_action( 'wp_enqueue_scripts', 'shapla_portfolio_scripts' );
 
 /**
  * Make the "read more" link to the post
  */
-function shapla_portfolio_excerpt_more( $more ) {
+function shapla_portfolio_excerpt_more() {
 	return ' <a class="read-more" href="' . get_permalink( get_the_ID() ) . '">' . __( 'Read More', 'shapla-portfolio' ) . '</a>';
 }
+
 add_filter( 'excerpt_more', 'shapla_portfolio_excerpt_more' );
 
 /**
  * Custom template tags for this theme.
  */
-require get_template_directory() . '/inc/init.php';
+require dirname( __FILE__ ) . '/inc/init.php';
--- inc/init.php
+++ inc/init.php
@@ -3,40 +3,40 @@
 /**
  * Load Theme Custom Widgets
  */
-require get_template_directory() . '/inc/widgets/widget-recent-projects.php';
-require get_template_directory() . '/inc/widgets/widget-featured-blog-posts.php';
-require get_template_directory() . '/inc/widgets/widget-slide.php';
-require get_template_directory() . '/inc/widgets/widget-cta.php';
-require get_template_directory() . '/inc/widgets/widget-contact.php';
-require get_template_directory() . '/inc/widgets/widget-hero.php';
-require get_template_directory() . '/inc/widgets/widget-intro.php';
+require dirname( __FILE__ ) . '/widgets/widget-recent-projects.php';
+require dirname( __FILE__ ) . '/widgets/widget-featured-blog-posts.php';
+require dirname( __FILE__ ) . '/widgets/widget-slide.php';
+require dirname( __FILE__ ) . '/widgets/widget-cta.php';
+require dirname( __FILE__ ) . '/widgets/widget-contact.php';
+require dirname( __FILE__ ) . '/widgets/widget-hero.php';
+require dirname( __FILE__ ) . '/widgets/widget-intro.php';
 
 /**
  * Custom template tags for this theme.
  */
-require get_template_directory() . '/inc/template-tags.php';
+require dirname( __FILE__ ) . '/template-tags.php';
 
 /**
  * Custom functions that act independently of the theme templates.
  */
-require get_template_directory() . '/inc/extras.php';
+require dirname( __FILE__ ) . '/extras.php';
 
 /**
  * Load Jetpack compatibility file.
  */
-require get_template_directory() . '/inc/jetpack.php';
+require dirname( __FILE__ ) . '/jetpack.php';
 
 /**
  * Customizer additions.
  */
-require get_template_directory() . '/inc/options/customizer.php';
+require dirname( __FILE__ ) . '/options/customizer.php';
 
 /**
  * Register Bootstrap Navigation Walker
  */
-require_once get_template_directory() . '/inc/framework/wp_bootstrap_navwalker.php';
+require_once dirname( __FILE__ ) . '/framework/class-wp-bootstrap-navwalker.php';
 
 /**
  * TGM Plugin Activation
  */
-require_once get_template_directory() . '/inc/framework/class-tgm-plugin-activation.php';
+require_once dirname( __FILE__ ) . '/framework/class-tgm-plugin-activation.php';
